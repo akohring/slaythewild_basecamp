b1b9bf628b7aaac625d4992b7148146c5d463486
akohring <ajkohring@gmail.com>
2018-02-11 23:29:58 -0500

    Complete implementation of write func

--- domain/image.go
+++ domain/image.go
@@ -1,15 +1,32 @@
 package domain
 
 import (
+	"io/ioutil"
+	"path/filepath"
+	"time"
 	"os"
 )
 
 // Image structure
 type Image struct {
 	directory string
+	file string
+	url string
+	displayTime string
 }
 
-// Write will write the image bytes to disk
-func Write(image Image, bytes []byte) {
-	os.MkdirAll(image.directory, os.ModePerm)
+const (
+	fileExtension = ".jpg"
+)
+
+// Write the image bytes to disk under the specified directory
+func Write(directory string, bytes []byte) Image {
+	os.MkdirAll(directory, os.ModePerm)
+	now := time.Now().UTC()
+	fileName := now.Format(time.RFC3339) + fileExtension
+	displayTime := now.Format("Mon Jan 02 2006 03:04:05 PM")
+	file := filepath.Join(directory, fileName)
+	url := "/image/"+fileName
+	ioutil.WriteFile(file, bytes, 0644)
+	return Image{directory: directory, file: file, url: url, displayTime: displayTime}
 }
--- domain/image_test.go
+++ domain/image_test.go
@@ -10,9 +10,15 @@ import (
 func TestWrite(t *testing.T) {
     imageDir := filepath.Join(os.TempDir(), "image_dir_test")
     defer os.RemoveAll(imageDir)
-    image := Image{directory: imageDir}
 
-    Write(image, []byte{0xFF, 0xD8, 0xFF, 0x00, 0xFF, 0xD9})
+    image := Write(imageDir, []byte{0xFF, 0xD8, 0xFF, 0x00, 0xFF, 0xD9})
+    assert.Equal(t, imageDir, image.directory)
+    assert.Equal(t, "/image/"+filepath.Base(image.file), image.url)
+    assert.NotNil(t, image.displayTime)
+    
     _, err := os.Stat(imageDir)
-    assert.True(t, err == nil)
+    assert.Nil(t, err)
+
+    _, err = os.Stat(image.file)
+    assert.Nil(t, err)
 }
