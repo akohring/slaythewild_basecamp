c4a1dc63a03813d6db4415bbe183c99225ab19df
akohring <ajkohring@gmail.com>
2018-02-25 23:14:33 -0500

    Added domain package to handle image writing and fetching

--- domain/image.go
+++ domain/image.go
@@ -1,10 +1,12 @@
 package domain
 
 import (
+	"fmt"
 	"io/ioutil"
-	"path/filepath"
-	"time"
 	"os"
+	"path/filepath"
+	"sort"
+	"time"
 )
 
 // Image structure
@@ -21,12 +23,37 @@ const (
 
 // Write the image bytes to disk under the specified directory
 func Write(directory string, bytes []byte) Image {
-	os.MkdirAll(directory, os.ModePerm)
 	now := time.Now().UTC()
 	fileName := now.Format(time.RFC3339) + fileExtension
-	displayTime := now.Format("Mon Jan 02 2006 03:04:05 PM")
+	image := newImage(directory, fileName)
+	os.MkdirAll(directory, os.ModePerm)
+	ioutil.WriteFile(image.file, bytes, 0644)
+	return image
+}
+
+// First image within a directory
+func First(directory string) (Image, error) {
+	return getFileByIndex(directory, 0)
+}
+
+func newImage(directory string, fileName string) Image {
 	file := filepath.Join(directory, fileName)
 	url := "/image/"+fileName
-	ioutil.WriteFile(file, bytes, 0644)
+	t, _ := time.Parse(time.RFC3339, fileName[0:len(fileName)-len(fileExtension)])
+	displayTime := t.Format("Mon Jan 02 2006 03:04:05 PM")
 	return Image{directory: directory, file: file, url: url, displayTime: displayTime}
+}
+
+func getFileByIndex(directory string, index int) (Image, error) {
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+       return Image{}, err
+	}
+	if(len(files) == 0) {
+		return Image{}, fmt.Errorf("Image directory is empty; imageDir=%s", directory)
+	}
+	sort.Slice(files, func(i,j int) bool{
+		return files[i].Name() < files[j].Name()
+	})
+	return newImage(directory, files[index].Name()), nil
 }
--- domain/image_test.go
+++ domain/image_test.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+    "fmt"
+	"io/ioutil"
 	"github.com/stretchr/testify/assert"
     "os"
     "path/filepath"
@@ -21,4 +23,35 @@ func TestWrite(t *testing.T) {
 
     _, err = os.Stat(image.file)
     assert.Nil(t, err)
+}
+
+func TestFirst(t *testing.T) {
+    imageDir := filepath.Join(os.TempDir(), "image_dir_test")
+    os.MkdirAll(imageDir, os.ModePerm)
+    defer os.RemoveAll(imageDir)
+
+    bytes := []byte{0xFF, 0xD8, 0xFF, 0x00, 0xFF, 0xD9}
+    files := []string{"2015.jpg", "2016.jpg", "2017.jpg", "2014.jpg"}
+    for _, file := range files {
+        ioutil.WriteFile(filepath.Join(imageDir, file), bytes, 0644)
+    }
+
+    image, err := First(imageDir)
+    assert.Nil(t, err)
+    assert.Equal(t, filepath.Join(imageDir, "2014.jpg"), image.file)
+}
+
+func TestFirstErrorWhenDirectoryDoesNotExist(t *testing.T) {
+    imageDir := filepath.Join(os.TempDir(), "image_dir_test")
+    _, err := First(imageDir)
+    assert.Equal(t, fmt.Sprintf("open %s: no such file or directory", imageDir), err.Error())
+}
+
+func TestFirstErrorWhenDirectoryIsEmpty(t *testing.T) {
+    imageDir := filepath.Join(os.TempDir(), "image_dir_test")
+    os.MkdirAll(imageDir, os.ModePerm)
+    defer os.RemoveAll(imageDir)
+
+    _, err := First(imageDir)
+    assert.Equal(t, fmt.Sprintf("Image directory is empty; imageDir=%s", imageDir), err.Error())
 }
